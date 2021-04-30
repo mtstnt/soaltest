af906e8d4e8de7e3be2f71e2a36674706947fcfd
mtstnt <matthew.sutanto0612@yahoo.com>
2021-05-01 01:41:20 +0700

    Ganti timezone jadi Asia/Jakarta dan routes utk Pembelian

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BarangController;
 use App\Http\Controllers\PembelianController;
 use App\Models\Barang;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,15 +19,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route("barang.index");
 });
 
 Route::prefix("auth")->middleware('not-auth')->group(function() {
     Route::get("/", function() {
         return redirect()->route('auth.login');
     });
+    
     Route::get("login", [AuthController::class, "login"])->name("auth.login");
     Route::post("login", [AuthController::class, "checkLogin"])->name("auth.check_login");
+    Route::post("logout", [AuthController::class, "logout"])->name("auth.logout");
 });
 
 Route::resource("barang", BarangController::class)->middleware('auth');
@@ -37,7 +40,8 @@ Route::prefix("pembelian")->middleware('auth')->group(function() {
     });
 
     Route::get("index", [PembelianController::class, "index"])->name("pembelian.index");
-    Route::get("create", [PembelianController::class, "index"])->name("pembelian.create");
-    Route::post("/", [PembelianController::class, "index"])->name("pembelian.store");
-    Route::delete("/{id}", [PembelianController::class, "index"])->name("pembelian.destroy");
+    Route::get("create", [PembelianController::class, "create"])->name("pembelian.create");
+    Route::get("/{id}", [PembelianController::class, "show"])->name("pembelian.show");
+    Route::post("/", [PembelianController::class, "store"])->name("pembelian.store");
+    Route::delete("/{id}", [PembelianController::class, "destroy"])->name("pembelian.destroy");
 });
