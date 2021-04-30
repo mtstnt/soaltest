9909658c208956cd246ea69bdfcabad4258e8d1c
mtstnt <matthew.sutanto0612@yahoo.com>
2021-04-30 18:05:46 +0700

    Controller resource barang dan model barang

---
+++ app/Http/Controllers/BarangController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Barang;
+use Illuminate\Http\Request;
+
+class BarangController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $allBarang = Barang::all();
+
+        return view("barang.index", [
+            'title' => "Admin: Master Barang",
+            'list_barang' => $allBarang,
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view("barang.create", [
+            "title" => "Admin: Menambahkan Barang"
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $requestData = $request->validate([
+            'id' => 'required|unique:master_barang,id|max:50',
+            'nama' => 'required|max:150',
+            'harga' => 'required|numeric',
+            'stok' => 'required|numeric',
+        ]);
+
+        if (! $barang = Barang::create($requestData)) {
+            return redirect()->to('barang.index');
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
