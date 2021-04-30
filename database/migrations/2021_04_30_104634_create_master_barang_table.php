9935aa96b8b844e6354ede320587f4825a9aa0fb
mtstnt <matthew.sutanto0612@yahoo.com>
2021-04-30 18:06:09 +0700

    Migrasi database master barang

---
+++ database/migrations/2021_04_30_104634_create_master_barang_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMasterBarangTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('master_barang', function (Blueprint $table) {
+            $table->string("id", 50)->primary()->unique();
+            $table->string("nama");
+            $table->unsignedInteger("harga");
+            $table->unsignedInteger("stok");
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('master_barang');
+    }
+}
