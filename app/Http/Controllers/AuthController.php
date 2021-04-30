463ad0f67c7dbb17c1445d3ac7204d1dfc827d1a
mtstnt <matthew.sutanto0612@yahoo.com>
2021-04-30 17:27:08 +0700

    AuthController login dan update auth/login view

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    //
+    public function login()
+    {
+        return view('auth.login');
+    }
+
+    public function checkLogin(Request $request)
+    {
+        # Diberikan max password length supaya hash tdk terlalu lambat
+        #  apabila ada yang iseng password diisi panjang.
+        $requestData = $request->validate([
+            'username' => 'required|exists:users,username',
+            'password' => 'required|max:16',
+        ]);
+
+        if (!Auth::attempt($requestData)) {
+            return \redirect()->route('auth.login')->with("err", "Invalid credentials!");
+        }
+
+        return \redirect()->route('admin.barang');
+    }
 }
