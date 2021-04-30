8e220e06980e7ff9c7ee008929fe066444e72a72
mtstnt <matthew.sutanto0612@yahoo.com>
2021-04-30 17:02:10 +0700

    Users migration, user model, konfigurasi Schema default string length in AppServiceProvider

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,8 +17,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name',
-        'email',
+        'username',
         'password',
     ];
 
@@ -29,7 +28,6 @@ class User extends Authenticatable
      */
     protected $hidden = [
         'password',
-        'remember_token',
     ];
 
     /**
@@ -37,7 +35,4 @@ class User extends Authenticatable
      *
      * @var array
      */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
 }
